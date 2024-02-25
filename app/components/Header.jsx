67fe0f437b45a1cd42e2f6bf73b086b9d56406fb
Nicholas Mutale <nicomutale@gmail.com>
2024-02-25 10:48:38 +0000

    Add description and readme file

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,8 +1,12 @@
 'use client'
+
+import Description from "./Description";
+
 const Header = () => {
     return (
       <header className="py-8">
         <h1 className="text-4xl font-bold text-center">Explore Mars Rover Photos</h1>
+        <Description~ />
       </header>
     );
   };
